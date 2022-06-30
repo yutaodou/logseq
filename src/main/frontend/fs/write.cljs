d07d9c57e8ce0e7a86adf3949d9600b454bbfef0
Tienson Qin <tiensonqin@gmail.com>
2022-06-30 20:07:17 +0800

    fix: outliner db not updated

--- src/main/frontend/fs/write.cljs
+++ src/main/frontend/fs/write.cljs
@@ -76,7 +76,8 @@
          (db/set-file-last-modified-at! repo path mtime)
          (db/set-file-content! repo path merged-content)
 
-         (when (or (and merged-doc (not= merged-content content))
+         (when (or (and merged-doc
+                        (not= merged-content db-content))
                    (not merged-doc))
            (state/pub-event! [:graph/reset-file repo path merged-content opts]))
 
