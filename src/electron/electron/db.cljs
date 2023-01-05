a6daf0ffe975575c97c2beb006d93269d3f2a440
Tienson Qin <tiensonqin@gmail.com>
2023-01-05 23:07:09 +0800

    downgrade yarn

--- src/electron/electron/db.cljs
+++ src/electron/electron/db.cljs
@@ -34,6 +34,8 @@
 
 (defn transact!
   [repo tx-data-meta]
+  (prn "transact!")
+  (clojure.pprint/pprint tx-data-meta)
   (let [config (get-db-config repo)]
     (when-not (.databaseExists datahike config)
       (create-database! repo))
@@ -57,7 +59,42 @@
     (str
      '[:find ?n
        :where
-       [?e :block/page ?p]
        [?p :block/name ?n]])
     (bean/->js [["db", config]]))
+
+  (def tx-data
+    '([:db/retract 19 :block/properties-text-values]
+      [:db/retract 19 :block/alias]
+      [:db/retract 19 :block/warning]
+      [:db/retract 19 :block/pre-block?]
+      [:db/retract 19 :block/priority]
+      [:db/retract 19 :block/invalid-properties]
+      [:db/retract 19 :block/properties]
+      [:db/retract 19 :block/updated-at]
+      [:db/retract 19 :block/repeated?]
+      [:db/retract 19 :block/refs]
+      [:db/retract 19 :block/scheduled]
+      [:db/retract 19 :block/properties-order]
+      [:db/retract 19 :block/created-at]
+      [:db/retract 19 :block/deadline]
+      [:db/retract 19 :block/tags]
+      [:db/retract 19 :block/path-refs]
+      [:db/retract 19 :block/type]
+      [:db/retract 19 :block/marker]
+      {:db/id 18, :block/updated-at 1672928886369}
+      {:block/uuid #uuid "63b69aea-33ee-45e9-acb1-83cdca2b9ed9",
+       :block/properties {},
+       :block/journal? true,
+       :block/left {:db/id 18},
+       :block/refs (),
+       :block/journal-day 20230105,
+       :block/format :markdown,
+       :block/tags (),
+       :block/content "nice",
+       :db/id 19,
+       :block/macros (),
+       :block/path-refs (),
+       :block/parent {:db/id 18},
+       :block/unordered true,
+       :block/page {:db/id 18}}))
   )
