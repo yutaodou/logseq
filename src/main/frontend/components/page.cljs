ae50bf7c4a33aabfa3bd4a8960c4759823fbd827
Tienson Qin <tiensonqin@gmail.com>
2024-12-06 15:56:35 +0800

    Use gap instead of margin-bottom

--- src/main/frontend/components/page.cljs
+++ src/main/frontend/components/page.cljs
@@ -654,7 +654,7 @@
 
            (if (and whiteboard-page? (not sidebar?))
              [:div ((state/get-component :whiteboard/tldraw-preview) (:block/uuid page))] ;; FIXME: this is not reactive
-             [:div.relative.grid.gap-2.page-inner
+             [:div.relative.grid.gap-4.page-inner
               (when (or (and db-based? (not block?))
                         (and (not db-based?) (not sidebar?) (not block?)))
                 [:div.flex.flex-row.space-between
